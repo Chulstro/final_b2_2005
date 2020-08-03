2ce5257869a2e3ec25e319d9eeefc46bb2f84722
Chandler Hulstrom <chulstro11@gmail.com>
2020-08-03 10:21:39 -0600

    Test for flight age count created

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe "When visiting a flights show page" do
     airline_1 = Airline.create(name: "Totally not Falling")
     @flight_1 = airline_1.flights.create(number: 12, date: "08/24/2020", departure_city: "Los Angeles", arrival_city: "Portland")
     @flight_1.passengers.create(name: "Jerrod", age: 14)
+    @flight_1.passengers.create(name: "Jennine", age: 24)
+    @flight_1.passengers.create(name: "Harold", age: 34)
+    @flight_1.passengers.create(name: "Samantha", age: 18)
+    @flight_1.passengers.create(name: "Ash", age: 9)
   end
   it "I see all of that flights info as well as airline and passengers" do
     visit "/flights/#{@flight_1.id}"
@@ -15,5 +19,12 @@ RSpec.describe "When visiting a flights show page" do
     expect(page).to have_content("Portland")
     expect(page).to have_content("Totally not Falling")
     expect(page).to have_content("Jerrod")
+  end
+
+  it "has a count of all minors and adults on the flight" do
+    visit "/flights/#{@flight_1.id}"
+
+    expect(page).to have_content("Minors Aboard: 2")
+    expect(page).to have_content("Adults Aboard: 3")
   end
 end
